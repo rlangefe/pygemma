a0387891bd15e9df0db5e6cb162ab157b9ba449b
Robert Langefeld <langrc18@wfu.edu>
2023-03-20 17:58:09 -0400

    Short updates to function; started writing method

--- pygemma/pygemma.py
+++ pygemma/pygemma.py
@@ -1,6 +1,18 @@
+import numpy as np
 from pygemma import pygemma_model
 
 # TODO: Implement GEMMA model call
 # See https://github.com/genetics-statistics/GEMMA/blob/master/src/lmm.cpp#L2208
 def pygemma():
-    pass
+    UtW = np.dot(U.T W)
+    UtY = np.dot(U.T, Y)
+
+    pygemma_model.CalcLambda('L', eval, UtW, &UtY_col.vector, cPar.l_min, cPar.l_max,
+                   cPar.n_region, cPar.l_mle_null, cPar.logl_mle_H0)
+    
+    pygemma_model.CalcLmmVgVeBeta(eval, UtW, &UtY_col.vector, cPar.l_mle_null,
+                        cPar.vg_mle_null, cPar.ve_mle_null, &beta.vector,
+                        &se_beta.vector)
+    
+    
+
--- pygemma/pygemma_model.pyx
+++ pygemma/pygemma_model.pyx
@@ -1,11 +1,3 @@
 import numpy as np
 
-cdef likelihood(double lamb,
-                double tau,
-                np.ndarray[double, ndim=1] alpha,
-                double beta,
-                np.ndarray[double, ndim=2] W,
-                np.ndarray[double, ndim=1] x):
-    
-
-cdef likelihood_restricted():
+CalcLambda
