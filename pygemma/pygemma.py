1d10bf270ba98af19025cfab89a88ffe5ff1ac2a
Robert C. Langefeld <rlangefe@umich.edu>
2023-03-24 18:42:13 -0400

    Close to working Still need to 	- Implement Newton-Raphson with scipy (not bad) 	- Loop over intervals for solver 	- Figure out how to estimate the standard error 	- Implement tests for those functions 	- Run with original GEMMA program 	- Compare results

--- pygemma/pygemma.py
+++ pygemma/pygemma.py
@@ -6,9 +6,14 @@ from scipy import optimize
 # TODO: Implement GEMMA model call
 # See https://github.com/genetics-statistics/GEMMA/blob/master/src/lmm.cpp#L2208
 def pygemma(Y, X, W, K):
-    eigenVals, U = np.linalg.eig(K)
+    eigenVals, U = np.linalg.eig(K) # Perform eigendecomposition
 
 
+
+    for g in X.shape[1]:
+        lambda_full = calc_lambda(eigenVals, U, Y, W, X[:,g])
+
+    # TODO: Optimize by precomputing Px
 
 
 def compute_Px(eigenVals, U, W, x, lam):
@@ -60,12 +65,105 @@ def likelihood_derivative2_lambda(lam, eigenVals, U, Y, W, x):
 
     yT_Px_G_Px_y = (yT_Px_y - yT_Px_Px_y)/lam
 
-    result = result - (n*n/2) * (yT_Px_G_Px_G_Px_y @ yT_Px_y - yT_Px_G_Px_y @ yT_Px_G_Px_y) / (yT_Px_y @ yT_Px_y)
+    result = result - n * (yT_Px_G_Px_G_Px_y @ yT_Px_y - yT_Px_G_Px_y @ yT_Px_G_Px_y) / (yT_Px_y * yT_Px_y)
+
+    return np.float32(result)
+
+def likelihood_derivative1_restricted_lambda(lam, eigenVals, U, Y, W, x):
+    n = W.shape[0]
+    c = W.shape[1] + 1
+
+    Px = compute_Px(eigenVals, U, W, x, lam)
+
+    result = -0.5*((n-c-1 - np.trace(Px))/lam)
+
+    yT_Px_y = Y.T @ Px @ Y
+
+    yT_Px_G_Px_y = (yT_Px_y - (Y.T @ Px) @ (Px @ Y))/lam
+
+    result = result - 0.5*(n - c - 1)*yT_Px_G_Px_y/yT_Px_y
+
+    return np.float32(result)
+
+def likelihood_derivative2_restricted_lambda(lam, eigenVals, U, Y, W, x): 
+    n = Y.shape[0]
+    c = W.shape[1] + 1
+
+    Px = compute_Px(eigenVals, U, W, x, lam)
+
+    yT_Px_y = Y.T @ Px @ Y
+
+    yT_Px_Px_y = (Y.T @ Px) @ (Px @ Y)
+
+    yT_Px_G_Px_G_Px_y = (yT_Px_y + (Y.T @ Px) @ Px @ (Px @ Y) - 2*yT_Px_Px_y)/(lam*lam)
+
+    yT_Px_G_Px_y = (yT_Px_y - yT_Px_Px_y)/lam
+
+    result = 0.5*(n - c - 1 + np.trace(Px @ Px) - 2*np.trace(Px))/(lam*lam)
+
+    result = result - (n - c - 1) * (yT_Px_G_Px_G_Px_y @ yT_Px_y - yT_Px_G_Px_y @ yT_Px_G_Px_y) / (yT_Px_y * yT_Px_y)
+
+    return np.float32(result)
+
+def likelihood(lam, tau, beta, eigenVals, U, Y, W, x):
+    n = W.shape[0]
+
+    W_x = np.c_[W,x]
+
+    H_inv = U @ np.diagflat(1/(lam*eigenVals + 1.0)) @ U.T
+
+    result = (n/2)*np.log(tau) 
+    result = result - (n/2)*np.log(2*np.pi)
+    
+    result = result - 0.5 * np.sum(np.log(lam*eigenVals + 1.0))
+
+    y_Wx_beta = Y - W_x @ beta
+
+    result = result - 0.5 * tau * y_Wx_beta.T @ H_inv @ y_Wx_beta
+
+
+    return np.float32(result)
+
+def likelihood_restricted(lam, tau, eigenVals, U, Y, W, x):
+    W_x = np.c_[W,x]
+
+    n, c = W_x.shape
+
+    H_inv = U @ np.diagflat(1/(lam*eigenVals + 1.0)) @ U.T
+
+    result = 0.5*(n - c - 1)*np.log(tau)
+    result = result - 0.5*(n - c - 1)*np.log(2*np.pi)
+    result = result + 0.5*np.log(np.linalg.det(W_x.T @ W_x))
+
+    result = result - 0.5 * np.sum(np.log(lam*eigenVals + 1.0))
+
+    result = result - 0.5*np.log(np.linalg.det(W_x.T @ H_inv @ W_x)) # Possible bottleneck
+
+    result = result - 0.5*(n - c - 1)*np.log(Y.T @ compute_Px(eigenVals, U, W, x, lam) @ Y)
+
+    return np.float32(result)
+
+def likelihood_restricted_lambda(lam, eigenVals, U, Y, W, x):
+    n, c = W.shape
+
+    W_x = np.c_[W,x]
+
+    H_inv = U @ np.diagflat(1/(lam*eigenVals + 1.0)) @ U.T
+
+    result = 0.5*(n - c - 1)*np.log(0.5*(n - c - 1)/np.pi)
+    result = result - 0.5*(n - c - 1)
+    result = result + 0.5*np.log(np.linalg.det(W_x.T @ W_x))
+
+    result = result - 0.5 * np.sum(np.log(lam*eigenVals + 1.0))
+
+    result = result - 0.5*np.log(np.linalg.det(W_x.T @ H_inv @ W_x)) # Possible bottleneck
+
+    result = result - 0.5*(n - c - 1)*np.log(Y.T @ compute_Px(eigenVals, U, W, x, lam) @ Y)
 
     return np.float32(result)
 
 
-def CalcLambda(eigenVals, U, Y, W, x):
+def calc_lambda(eigenVals, U, Y, W, x):
     # Loop over intervals and find where likelihood changes signs with respect to lambda
     lambda0 = np.power(10.0, -5.0)
     lambda1 = np.power(10.0, 5.0)
@@ -85,3 +183,33 @@ def CalcLambda(eigenVals, U, Y, W, x):
         lambda_min = lambda1
 
     return lambda_min
+
+def calc_lambda_restricted(eigenVals, U, Y, W, x):
+    # Loop over intervals and find where likelihood changes signs with respect to lambda
+    # TODO: Create range of values and loop over the brackets
+    lambda0 = np.power(10.0, -5.0)
+    lambda1 = np.power(10.0, 5.0)
+
+    likelihood_lambda0 = likelihood_derivative1_restricted_lambda(lambda0, eigenVals, U, Y, W, x)
+    likelihood_lambda1 = likelihood_derivative1_restricted_lambda(lambda1, eigenVals, U, Y, W, x)
+
+    if likelihood_lambda0*likelihood_lambda1 < 0:
+        lambda_min, _ = optimize.brentq(f=lambda l: likelihood_derivative1_restricted_lambda(l, eigenVals, U, Y, W, x), 
+                                            a=lambda0, 
+                                            b=lambda1,
+                                            xtol=0.1,
+                                            maxiter=5000)
+        
+        # TODO: Add Newton-Raphson iterations (scipy.optimize.newton) and catch error for no convergance on brent
+        
+
+    elif likelihood_lambda0 < 0:
+        lambda_min = lambda0
+    else:
+        lambda_min = lambda1
+
+    return lambda_min
+
+# TODO: Implement unrestricted lambda estimates like with the calc_lambda_restricted function
+
+
--- tests/test_pygemma.py
+++ tests/test_pygemma.py
@@ -1,8 +1,12 @@
-import numpy as np
-from rich.console import Console
 import time
+import os
+
+import numpy as np
+import pandas as pd
 
 import pygemma
+
+from rich.console import Console
 
 console = Console()
 
@@ -17,7 +21,7 @@ def run_function_test(function, parameters):
         print(e)
         failed = True
         
-    diff = str(round(time.time() - start))
+    diff = str(round(time.time() - start, 4))
     if failed:
         console.log(f"[red]Failed {function.__name__} - {diff} s")
         return 1
@@ -34,14 +38,7 @@ def run_test_list(functions_and_args):
     console.log(f"Failed {failures} out of {len(functions_and_args)} tests")
 
 
-with console.status("[bold green]Running pyGEMMA Function Tests...") as status:
-
-    # Seed tests
-    np.random.seed(42)
-
-    # Initializing parameters for tests
-    n = 1000
-    covars = 10
+def generate_test_matrices(n=1000, covars=10):
     K = np.random.uniform(size=(n, n))
     K = np.abs(np.tril(K) + np.tril(K, -1).T)
     K = np.dot(K, K.T)
@@ -52,16 +49,76 @@ with console.status("[bold green]Running pyGEMMA Function Tests...") as status:
                         replace=True)
     Y = np.random.rand(n, 1)
     lam = 5
+    tau = 10
+    beta = np.random.rand(covars+1, 1)
 
-    console.log(f'Test Parameters: n={n}, lam={lam}')
+    return x, Y, W, eigenVals, U, lam, beta, tau
+
+with console.status("[bold green]Running pyGEMMA Function Run Tests...") as status:
+
+    # Seed tests
+    np.random.seed(42)
+
+    n = 1000
+    covars = 10
+
+    # Initializing parameters for tests
+    x, Y, W, eigenVals, U, lam, beta, tau = generate_test_matrices(n=n, covars=covars)
+    
+    console.log(f'Test Parameters: n={n}, lam={lam}, tau={tau}')
     
     functions_and_args = [
                             (pygemma.compute_Px, [eigenVals, U, W, x, lam]),
                             (pygemma.likelihood_lambda, [lam, eigenVals, U, Y, W, x]),
                             (pygemma.likelihood_derivative1_lambda, [lam, eigenVals, U, Y, W, x]),
                             (pygemma.likelihood_derivative2_lambda, [lam, eigenVals, U, Y, W, x]),
-                            (pygemma.CalcLambda, [eigenVals, U, Y, W, x])
+                            (pygemma.calc_lambda, [eigenVals, U, Y, W, x]),
+                            (pygemma.calc_lambda_restricted, [eigenVals, U, Y, W, x]),
+                            (pygemma.likelihood, [lam, tau, beta, eigenVals, U, Y, W, x]),
+                            (pygemma.likelihood_restricted, [lam, tau, eigenVals, U, Y, W, x]),
+                            (pygemma.likelihood_restricted_lambda, [lam, eigenVals, U, Y, W, x]),
+                            (pygemma.likelihood_derivative1_restricted_lambda, [lam, eigenVals, U, Y, W, x]),
+                            (pygemma.likelihood_derivative2_restricted_lambda, [lam, eigenVals, U, Y, W, x]),
                           ]
     
-    run_test_list(functions_and_args)    
+    run_test_list(functions_and_args)
+
+DATADIR = r"C:\Users\lange\Desktop\School\Michigan\MS Year 1\Spring\BIOSTAT 666\Final Project\pygemma\data"
+
+dataset_list = [
+        {
+            'name'   : 'mouse_hs1940',
+            'snps'   : os.path.join(DATADIR, "mouse_hs1940_snps.txt"),
+            'covars' : None,
+            'pheno'  : os.path.join(DATADIR, "mouse_hs1940.txt"),
+            
+        }
+    ]
+
+exit(0)
+
+for dataset in dataset_list:
+    dataset_name = dataset['name']
+
+    Y = 
+
+    with console.status("[bold green]Running pyGEMMA Function Tests - {dataset_name}...") as status:
+
+        # Seed tests
+        np.random.seed(42)
+
+        # Initializing parameters for tests
+        x, Y, W, eigenVals, U, lam = generate_test_matrices(n=1000, covars=10)
+        
+        console.log(f'Test Parameters: n={n}, lam={lam}')
+        
+        functions_and_args = [
+                                (pygemma.compute_Px, [eigenVals, U, W, x, lam]),
+                                (pygemma.likelihood_lambda, [lam, eigenVals, U, Y, W, x]),
+                                (pygemma.likelihood_derivative1_lambda, [lam, eigenVals, U, Y, W, x]),
+                                (pygemma.likelihood_derivative2_lambda, [lam, eigenVals, U, Y, W, x]),
+                                (pygemma.calc_lambda, [eigenVals, U, Y, W, x])
+                            ]
+        
+        run_test_list(functions_and_args)
 
