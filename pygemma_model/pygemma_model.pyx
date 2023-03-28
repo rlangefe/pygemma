ffdd9ad12cd9fbe3e51c9cc42f6054b09eaf1fb8
Robert Langefeld <rlangefe@mulan.csgstat.sph.umich.edu>
2023-03-28 18:04:56 -0400

    Revert "Fixed issue with lrt; should work now (hopefully)"
    This reverts commit 5bc22b1691a546f2079ddac7e140e28a12fbeaa9.

--- pygemma_model/pygemma_model.pyx
+++ pygemma_model/pygemma_model.pyx
@@ -6,8 +6,6 @@ from scipy import optimize, stats
 
 @cython.boundscheck(False) # compiler directive
 @cython.wraparound(False) # compiler directive
-@cython.nonecheck(False)
-@cython.cdivision(True)
 def compute_Pc(
                 np.ndarray[np.float32_t, ndim=1] eigenVals, 
                 np.ndarray[np.float32_t, ndim=2] U, 
@@ -23,11 +21,9 @@ def compute_Pc(
 # Reimplementing #
 ##################
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
-@cython.nonecheck(False)
-@cython.cdivision(True)
-def compute_Pc_cython(
+@cython.boundscheck(False) # compiler directive
+@cython.wraparound(False) # compiler directive
+cdef compute_Pc_cython(
                 np.ndarray[np.float32_t, ndim=1] eigenVals, 
                 np.ndarray[np.float32_t, ndim=2] U, 
                 np.ndarray[np.float32_t, ndim=2] W, 
@@ -38,10 +34,6 @@ def compute_Pc_cython(
 
     return H_inv - H_inv @ W @ np.linalg.inv(W.T @ H_inv @ W) @ W.T @ H_inv
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
-@cython.nonecheck(False)
-@cython.cdivision(True)
 def calc_beta_vg_ve(np.ndarray[np.float32_t, ndim=1] eigenVals,
                     np.ndarray[np.float32_t, ndim=2] U, 
                     np.ndarray[np.float32_t, ndim=2] W, 
@@ -68,10 +60,6 @@ def calc_beta_vg_ve(np.ndarray[np.float32_t, ndim=1] eigenVals,
 
     return beta, se_beta, tau
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
-@cython.nonecheck(False)
-@cython.cdivision(True)
 def calc_beta_vg_ve_restricted(np.ndarray[np.float32_t, ndim=1] eigenVals,
                     np.ndarray[np.float32_t, ndim=2] U, 
                     np.ndarray[np.float32_t, ndim=2] W, 
@@ -89,7 +77,7 @@ def calc_beta_vg_ve_restricted(np.ndarray[np.float32_t, ndim=1] eigenVals,
 
     cdef np.ndarray[np.float32_t, ndim=2] W_xt_Pc = W_x.T @ Pc
     cdef np.ndarray[np.float32_t, ndim=2] beta_vec = np.linalg.inv(W_xt_Pc @ W_x) @ (W_xt_Pc @ Y)
-    beta = float(beta_vec[c-1])
+    beta = float(beta_vec[-1])
 
     cdef np.float32_t ytPxy = Y.T @ Px @ Y
 
@@ -99,10 +87,6 @@ def calc_beta_vg_ve_restricted(np.ndarray[np.float32_t, ndim=1] eigenVals,
 
     return np.float32(beta), np.float32(se_beta), np.float32(tau)
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
-@cython.nonecheck(False)
-@cython.cdivision(True)
 def likelihood_lambda(np.float32_t lam,
                       np.ndarray[np.float32_t, ndim=1] eigenVals, 
                       np.ndarray[np.float32_t, ndim=2] U, 
@@ -120,10 +104,6 @@ def likelihood_lambda(np.float32_t lam,
 
     return np.float32(result)
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
-@cython.nonecheck(False)
-@cython.cdivision(True)
 def likelihood_derivative1_lambda(np.float32_t lam,
                                   np.ndarray[np.float32_t, ndim=1] eigenVals, 
                                   np.ndarray[np.float32_t, ndim=2] U, 
@@ -134,19 +114,15 @@ def likelihood_derivative1_lambda(np.float32_t lam,
     cdef np.float32_t result = -0.5*((n-np.sum(1/(lam*eigenVals + 1.0)))/lam)
 
     cdef np.ndarray[np.float32_t, ndim=2] Px = compute_Pc_cython(eigenVals, U, W, lam)
-    cdef np.ndarray[np.float32_t, ndim=2] yt_Px = Y.T @ Px
 
-    cdef np.float32_t yT_Px_y = yt_Px @ Y
+    cdef np.ndarray[np.float32_t, ndim=2] yT_Px_y = Y.T @ Px @ Y
 
-    cdef np.float32_t yT_Px_G_Px_y = (yT_Px_y - yt_Px @ yt_Px.T)/lam
+    cdef np.ndarray[np.float32_t, ndim=2] yT_Px_G_Px_y = (yT_Px_y - (Y.T @ Px) @ (Px @ Y))/lam
 
     result = result - (n/2)*yT_Px_G_Px_y/yT_Px_y
 
     return np.float32(result)
-@cython.boundscheck(False)
-@cython.wraparound(False)
-@cython.nonecheck(False)
-@cython.cdivision(True)
+
 def likelihood_derivative2_lambda(np.float32_t lam,
                                   np.ndarray[np.float32_t, ndim=1] eigenVals, 
                                   np.ndarray[np.float32_t, ndim=2] U, 
@@ -157,24 +133,19 @@ def likelihood_derivative2_lambda(np.float32_t lam,
     cdef np.float32_t result = 0.5*(n + np.sum(np.power(lam*eigenVals + 1.0, -2)) + 2*np.sum(np.power(lam*eigenVals + 1.0,-1)))
 
     cdef np.ndarray[np.float32_t, ndim=2] Px = compute_Pc_cython(eigenVals, U, W, lam)
-    cdef np.ndarray[np.float32_t, ndim=2] yt_Px = Y.T @ Px
 
-    cdef np.float32_t yT_Px_y = yt_Px @ Y
+    cdef np.ndarray[np.float32_t, ndim=2] yT_Px_y = Y.T @ Px @ Y
 
-    cdef np.float32_t yT_Px_Px_y = yt_Px @ yt_Px.T
+    cdef np.ndarray[np.float32_t, ndim=2] yT_Px_Px_y = (Y.T @ Px) @ (Px @ Y)
 
-    cdef np.float32_t yT_Px_G_Px_G_Px_y = (yT_Px_y + yt_Px @ Px @ yt_Px.T - 2*yT_Px_Px_y)/(lam*lam)
+    cdef np.ndarray[np.float32_t, ndim=2] yT_Px_G_Px_G_Px_y = (yT_Px_y + (Y.T @ Px) @ Px @ (Px @ Y) - 2*yT_Px_Px_y)/(lam*lam)
 
-    cdef np.float32_t yT_Px_G_Px_y = (yT_Px_y - yT_Px_Px_y)/lam
+    cdef np.ndarray[np.float32_t, ndim=2] yT_Px_G_Px_y = (yT_Px_y - yT_Px_Px_y)/lam
 
-    result = result - n * (yT_Px_G_Px_G_Px_y * yT_Px_y - yT_Px_G_Px_y * yT_Px_G_Px_y) / (yT_Px_y * yT_Px_y)
+    result = result - n * (yT_Px_G_Px_G_Px_y @ yT_Px_y - yT_Px_G_Px_y @ yT_Px_G_Px_y) / (yT_Px_y * yT_Px_y)
 
     return np.float32(result)
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
-@cython.nonecheck(False)
-@cython.cdivision(True)
 def likelihood_derivative1_restricted_lambda(np.float32_t lam,
                                   np.ndarray[np.float32_t, ndim=1] eigenVals, 
                                   np.ndarray[np.float32_t, ndim=2] U, 
@@ -184,23 +155,17 @@ def likelihood_derivative1_restricted_lambda(np.float32_t lam,
     c = W.shape[1]
 
     cdef np.ndarray[np.float32_t, ndim=2] Px = compute_Pc_cython(eigenVals, U, W, lam)
-    cdef np.ndarray[np.float32_t, ndim=2] yt_Px = Y.T @ Px
 
     cdef np.float32_t result = -0.5*((n-c - np.trace(Px))/lam)
 
-    cdef np.float32_t yT_Px_y = yt_Px @ Y
+    cdef np.ndarray[np.float32_t, ndim=2] yT_Px_y = Y.T @ Px @ Y
 
-    cdef np.float32_t yT_Px_G_Px_y = (yT_Px_y - yt_Px @ yt_Px.T)/lam
+    cdef np.ndarray[np.float32_t, ndim=2] yT_Px_G_Px_y = (yT_Px_y - (Y.T @ Px) @ (Px @ Y))/lam
 
     result = result - 0.5*(n - c)*yT_Px_G_Px_y/yT_Px_y
 
     return np.float32(result)
 
-
-@cython.boundscheck(False)
-@cython.wraparound(False)
-@cython.nonecheck(False)
-@cython.cdivision(True)
 def likelihood_derivative2_restricted_lambda(np.float32_t lam,
                                   np.ndarray[np.float32_t, ndim=1] eigenVals, 
                                   np.ndarray[np.float32_t, ndim=2] U, 
@@ -210,27 +175,21 @@ def likelihood_derivative2_restricted_lambda(np.float32_t lam,
     c = W.shape[1]
 
     cdef np.ndarray[np.float32_t, ndim=2] Px = compute_Pc_cython(eigenVals, U, W, lam)
-    cdef np.ndarray[np.float32_t, ndim=2] yt_Px = Y.T @ Px
 
-    cdef np.float32_t yT_Px_y = yt_Px @ Y
+    cdef np.ndarray[np.float32_t, ndim=2] yT_Px_y = Y.T @ Px @ Y
 
-    cdef np.float32_t yT_Px_Px_y = yt_Px @ yt_Px.T
+    cdef np.ndarray[np.float32_t, ndim=2] yT_Px_Px_y = (Y.T @ Px) @ (Px @ Y)
 
-    cdef np.float32_t yT_Px_G_Px_G_Px_y = (yT_Px_y + yt_Px @ Px @ yt_Px.T - 2*yT_Px_Px_y)/(lam*lam)
+    cdef np.ndarray[np.float32_t, ndim=2] yT_Px_G_Px_G_Px_y = (yT_Px_y + (Y.T @ Px) @ Px @ (Px @ Y) - 2*yT_Px_Px_y)/(lam*lam)
 
-    cdef np.float32_t yT_Px_G_Px_y = (yT_Px_y - yT_Px_Px_y)/lam
+    cdef np.ndarray[np.float32_t, ndim=2] yT_Px_G_Px_y = (yT_Px_y - yT_Px_Px_y)/lam
 
     cdef np.float32_t result = 0.5*(n - c + np.trace(Px @ Px) - 2*np.trace(Px))/(lam*lam)
 
-    result = result - (n - c) * (yT_Px_G_Px_G_Px_y * yT_Px_y - yT_Px_G_Px_y * yT_Px_G_Px_y) / (yT_Px_y * yT_Px_y)
+    result = result - (n - c) * (yT_Px_G_Px_G_Px_y @ yT_Px_y - yT_Px_G_Px_y @ yT_Px_G_Px_y) / (yT_Px_y * yT_Px_y)
 
     return np.float32(result)
 
-
-@cython.boundscheck(False)
-@cython.wraparound(False)
-@cython.nonecheck(False)
-@cython.cdivision(True)
 def likelihood(np.float32_t lam, 
                np.float32_t tau, 
                np.ndarray[np.float32_t, ndim=2] beta, 
@@ -254,11 +213,6 @@ def likelihood(np.float32_t lam,
 
     return np.float32(result)
 
-
-@cython.boundscheck(False)
-@cython.wraparound(False)
-@cython.nonecheck(False)
-@cython.cdivision(True)
 def likelihood_restricted(np.float32_t lam, 
                np.float32_t tau,
                np.ndarray[np.float32_t, ndim=1] eigenVals, 
@@ -285,11 +239,6 @@ def likelihood_restricted(np.float32_t lam,
 
     return np.float32(result)
 
-
-@cython.boundscheck(False)
-@cython.wraparound(False)
-@cython.nonecheck(False)
-@cython.cdivision(True)
 def likelihood_restricted_lambda(np.float32_t lam, 
                                  np.ndarray[np.float32_t, ndim=1] eigenVals, 
                                  np.ndarray[np.float32_t, ndim=2] U, 
